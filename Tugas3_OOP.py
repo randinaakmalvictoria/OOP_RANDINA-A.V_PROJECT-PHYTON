b5929bedba2a3ea654cd15911ef6fd7a214fc375
randinaakmalvictoria <randinaav5@gmail.com>
2026-04-08 10:40:31 +0700

    Add files via upload

---
+++ Tugas3_OOP.py
@@ -0,0 +1,34 @@
+a = "Toyota"
+b="Avanza"
+c=2022
+d="Hitam"
+e=250000000
+f="B 1234 CD"
+g="Bensin"
+h="1500cc"
+i=7
+j="Tersedia"
+
+print ("==== DATA KEMDARAAN ====")
+print ("merk                  :",a)
+print ("Tipe                  :",b)
+print ("tahun                 :",c)
+print ("Warna                 :",d)
+print ("Harga                 :",e)
+print ("No Polisi             :",f)
+print ("Bahan bakar           :",g)
+print ("Kapasitas Mesin       :",h)
+print ("Jumlah kursi          :",i)
+print ("status                :",j)
+print ("")
+print ("==== DATA KEMDARAAN (SETELAH UPDATE)====")
+print ("merk                  : Toyota")
+print ("Tipe                  : Avanza")
+print ("tahun                 : 2022")
+print ("Warna                 : Hitam")
+print ("Harga                 : 230000000")
+print ("No Polisi             : B 1234 CD")
+print ("Bahan bakar           : Bensin")
+print ("Kapasitas Mesin       : 1500cc")
+print ("Jumlah kursi          : 7")
+print ("status                : Terjual")
