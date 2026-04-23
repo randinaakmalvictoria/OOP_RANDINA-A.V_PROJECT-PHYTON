8923fc5f0347937107afd91f9ba169827daf2bad
randinaakmalvictoria <randinaav5@gmail.com>
2026-04-23 08:56:31 +0700

    Add files via upload

---
+++ tugas4.py
@@ -0,0 +1,15 @@
+
+nama=input("masukan nama kamu: ")
+print("",nama)
+
+umur=input("masukan umur: ")
+print("",umur)
+
+no_hp=input("masukan no_hp: ")
+print("",no_hp)
+
+email=input("masukan email: ")
+print("",email)
+
+alamat=input("masukan alamat:")
+print("",alamat)
